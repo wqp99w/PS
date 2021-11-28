1b59eb5ebbd5639b932377762e96c9fa2f449c3d
wqp99 <wqp99@LAPTOP-9VJD2QNN>
2021-11-28 19:50:35 +0900

    초기 커밋.

---
+++ 조우리/소스.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+
+
+int main() {
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	cin >> a >> b;
+	int a3 =b % 10;
+	int a4 = b % 100 / 10;
+	int a5 = b / 100;
+	cout << a*a3<<"\n";
+	sum = sum + a*a3;
+	cout << a * a4<<"\n";
+	sum = sum + a*10 * a4;
+	cout << a * a5 << "\n";
+	sum = sum + a*100 * a5;
+	cout << sum;
+	
+	
+}
